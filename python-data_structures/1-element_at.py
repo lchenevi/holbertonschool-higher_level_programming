aa29d57963a3c9dacaa5f9c4ba51e9a939f31fd6
lchenevi <leo.cheneviere@gmail.com>
2024-01-30 10:23:22 +0100

    syntax correction for full points on task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,9 +4,3 @@ def element_at(my_list, index):
         return None
     else:
         return my_list[index]
-'''
-my_list = [1, 2, 3, 4, 5]
-index = 3
-result = element_at(my_list, index)
-print("Element at index {:d} is {}".format(index, result))
-'''
