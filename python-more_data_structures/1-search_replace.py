c86220ea73ff0a632715290c1410540e009b590d
lchenevi <leo.cheneviere@gmail.com>
2024-02-01 10:40:15 +0100

    trying to find the issue for the last point

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = [replace if element == search else element for element
-                 in my_list]
-    return new_list
+    return [find if find != search else replace for find in my_list]
