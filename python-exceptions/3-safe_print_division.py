027650d17559774910b50856a796103e90a155b8
lchenevi <leo.cheneviere@gmail.com>
2024-02-02 19:37:54 +0100

    pushing task 4 and 5 along with correction of task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    nb_print = 0
+def safe_print_division(a, b):
+    res = 0
     try:
-        for i in range(x):
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end="")
-                nb_print += 1
-    except (IndexError, ValueError, TypeError):
-        pass  # Ignore exceptions
+        res = a / b
+    except ZeroDivisionError:
+        res = None
     finally:
-        print()
-        return nb_print
+        print("Inside result: {}".format(res))
+        return res
