382e7481e895970b4cfe2819217dca0fbb13f5fd
lchenevi <leo.cheneviere@gmail.com>
2024-02-07 22:40:46 +0100

    trying to fix my code

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,11 +3,10 @@
 
 
 class Square:
+    """Size validation of size in square"""
     def __init__(self, size=0):
-        """Initialize a Square instance with a given size."""
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+        if not type(size) is int:
+            raise Exception("size must be an integer")
+        if int(size) < 0:
+            raise Exception("size must be >= 0")
+        self.__size = size
