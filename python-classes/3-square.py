6629e14033fd896d73c6529b13d5399511291ac4
lchenevi <leo.cheneviere@gmail.com>
2024-02-07 22:47:45 +0100

    pushing modifications done on task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """Contains an class : 'Square'"""
 
+
 class Square:
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+        """Initialize a Square instance with a given size."""
+        try:
+            if not isinstance(size, int):
+                raise Exception('size must be an integer')
+            elif size < 0:
+                raise Exception('size must be >= 0')
+            else:
+                self.__size = size
+        except Exception as e:
+            print(e)
 
     def area(self):
-        size = self.__size
-        return size * size
+        '''Return the area of the square.'''
+        try:
+            return self.__size ** 2
+        except Exception as e:
+            print(e)
