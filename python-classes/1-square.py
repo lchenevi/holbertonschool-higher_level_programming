dbc0f3741a420d3fbb673bfa2d3d87fdc19ff422
lchenevi <leo.cheneviere@gmail.com>
2024-02-07 22:22:03 +0100

    trying to fix my code

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Contains an class : 'Square'"""
+
+
 class Square:
-    def __init__(self, size):
-        self.__size = size
+    """Private instance attribute: size"""
+    def __init__(self, x):
+        self.__size = x
