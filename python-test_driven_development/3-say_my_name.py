3d5fb1a3748b7448974604d347f2ee25b051c0d9
lchenevi <leo.cheneviere@gmail.com>
2024-02-09 00:29:27 +0100

    pushing task 3 and 4

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Print name surname
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    '''
+
+    Function who takes two string as arguments
+    and prints them in a sentence
+    '''
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Print square
+"""
+
+
+def print_square(size):
+    '''
+
+    Function that prints the
+    area of the square
+    '''
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for row in range(size):
+        print("#" * size, end="")
+        print()
