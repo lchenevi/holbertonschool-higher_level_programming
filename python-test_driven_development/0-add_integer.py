9decb1891bbdfe2f24f587f48623da3bfd6d7cfa
lchenevi <leo.cheneviere@gmail.com>
2024-02-08 13:44:12 +0100

    trying a diferent versqion of my code and pushing the test file that was forgoten

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,22 @@
+#!/usr/bin/python3
+"""
+Adds two numbers together.
+"""
+
+
 def add_integer(a, b=98):
-    '''
-    Adds two integers.
-
-    Args:
-        a (int or float): The first number.
-        b (int or float): The second number. Defaults to 98.
-
+    """
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
     Returns:
-        int: The addition of a and b as integers.
-    '''
-    if not isinstance(a, (int, float)):
-        raise TypeError('a must be an integer or b must be an integer')
-    if not isinstance(b, (int, float)):
-        raise TypeError('a must be an integer or b must be an integer')
-
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    int: The sum of x and y.
+    Example:
+    add(2, 3)
+    5
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a + b)
