9647cb8cb30718b87e847cb3b1c9fd89eb186657
lchenevi <leo.cheneviere@gmail.com>
2024-02-14 15:33:29 +0000

    aaaah

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -10,7 +10,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Represents a rectangle."""
-
     def __init__(self, width, height):
         super().__init__()
         self.__width = width
