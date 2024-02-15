97c1390a2377bf21f828ebf0d4b570460fbbd5ee
lchenevi <leo.cheneviere@gmail.com>
2024-02-15 11:37:04 +0000

    pushing task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,12 +3,7 @@
 
 
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')  # Print each line without
-                                     # adding extra newline characters
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')  # Print each line without
+                                 # adding extra newline characters
