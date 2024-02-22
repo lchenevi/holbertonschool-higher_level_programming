4419ec89e2609c4cdf1dc57ec5af8146758f218c
lchenevi <leo.cheneviere@gmail.com>
2024-02-22 11:00:14 +0100

    pushing the base test file and code

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,22 +1,61 @@
 #!/usr/bin/python3
+"""Create class Base"""
+import json
+
 
 class Base:
-    """Base class for managing id attribute in future classes."""
-
-    __nb_objects = 0  # Private class attribute
+    """Class Base"""
+    __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        Class constructor.
-
-        Args:
-            id (int): If provided, assigns the public instance
-                      attribute 'id' with the given value. Otherwise,
-                      increments '__nb_objects' and assigns the new
-                      value to 'id'
-        """
+        """Initialisation for instance of Base"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns JSON string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Converting a dict into a json string"""
+        if list_objs:
+            j = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        else:
+            j = '[]'
+        with open(cls.__name__ + '.json', 'w') as f:
+            f.write(j)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """json to string static method"""
+        if json_string:
+            return json.loads(json_string)
+        return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            instance = cls(5, 5)
+        else:
+            instance = cls(5)
+        instance.update(**dictionary)
+        return instance
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        try:
+            filename = cls.__name__ + '.json'
+            with open(filename, mode='r') as f:
+                d = cls.from_json_string(f.read())
+            return [cls.create(**x) for x in d]
+        except FileNotFoundError:
+            return []
