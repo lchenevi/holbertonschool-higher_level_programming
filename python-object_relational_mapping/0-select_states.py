0a46419e5b1cd6b529efd9c7d3a071c891c2f4f3
lchenevi <leo.cheneviere@gmail.com>
2024-03-20 10:57:12 +0100

    pushing task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,30 +2,34 @@
 """ script that lists all states
 from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    # Extract MySQL username, password, and database name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == '__main__':
+    """
+    Main execution block
+    Connects to the database and retrieves the states
+    """
+    # Connect to the MySQL database using the provided arguments
+    db = MySQLdb.connect(
+        host="localhost",  # Hostname of the database server
+        user=argv[1],     # Username obtained from command-line argument
+        port=3306,        # Port number of the database server
+        passwd=argv[2],   # Password obtained from command-line argument
+        db=argv[3]
+    )       # Database name obtained from command-line argument
 
-    # Connect to MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    # Create a cursor object to execute SQL queries
+    cur = db.cursor()
 
-    # Create a cursor object using cursor() method
-    cursor = db.cursor()
+    # Execute an SQL query to select all rows
+    # from the 'states' table
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Execute SQL query to select all states from the states table and sort by id in ascending order
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Fetch all rows returned by the query
+    rows = cur.fetchall()
 
-    # Fetch all the rows in a list of tuples
-    results = cursor.fetchall()
-
-    # Display results
-    for row in results:
+    # Iterate through the fetched rows and print each row
+    for row in rows:
         print(row)
-
-    # Disconnect from server
-    db.close()
